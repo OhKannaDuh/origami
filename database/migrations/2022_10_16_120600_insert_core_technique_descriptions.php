a38fac10b5f6766f0c1430b21bb94a7ce7f26f28
OhKannaDuh <liammcnair@outlook.com>
2022-10-16 15:01:13 +0100

    fix migration

--- database/migrations/2022_10_16_120600_insert_core_technique_descriptions.php
+++ database/migrations/2022_10_16_120600_insert_core_technique_descriptions.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Schema;
 
-return new() class extends Migration
+return new class extends Migration
 {
     private TechniqueRepositoryInterface $techniques;
 
