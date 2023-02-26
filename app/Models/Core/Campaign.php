52a7a9e27f18661f0174d2374a412bfed1b4f4cc
OhKannaDuh <liammcnair@outlook.com>
2023-02-26 15:29:09 +0000

    List adv and disadv

--- app/Models/Core/Campaign.php
+++ app/Models/Core/Campaign.php
@@ -3,7 +3,9 @@
 namespace App\Models\Core;
 
 use App\Behaviours\HasRepository;
+use App\Models\Character\Advantage;
 use App\Models\Character\Character;
+use App\Models\Character\Disadvantage;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
@@ -53,5 +55,22 @@ final class Campaign extends Model
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'campaign_user');
+    }
+
+
+    public function listAdvantagesAndDisadvantagess(): void
+    {
+        foreach ($this->characters as $character) {
+            assert($character instanceof Character);
+            foreach ($character->advantages as $advantage) {
+                assert($advantage instanceof Advantage);
+                echo $advantage->getKey() . ' - ' . $advantage->name . PHP_EOL;
+            }
+
+            foreach ($character->disadvantages as $disadvantage) {
+                assert($disadvantage instanceof Disadvantage);
+                echo $disadvantage->getKey() . ' - ' . $disadvantage->name . PHP_EOL;
+            }
+        }
     }
 }
