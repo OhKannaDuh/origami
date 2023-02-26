affef0ca19f8431801f42931c69e8bfacebadfd0
OhKannaDuh <liammcnair@outlook.com>
2023-02-26 11:58:09 +0000

    clean up

--- routes/web/web.php
+++ routes/web/web.php
@@ -1,15 +1,6 @@
 <?php
 
 use App\Http\Controllers\IndexController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-// Auth::routes(['reset' => false, 'verify' => false]);
-
 Route::get('/', [IndexController::class, 'show'])->name('index.show');
-Route::get('/parsed-techniques', function () {
-    $techniques = json_decode(file_get_contents(base_path('data/exports/parsed-techniques.json')), true);
-    return view('parsed-techniques', [
-        'techniques' => $techniques,
-    ]);
-});
